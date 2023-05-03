a8f3ceab0ac3f287c732d15c1bdf1a3178d7498a
felix1805 <felix1805@gmail.com>
2023-05-03 17:27:34 -0400

    API post request added, message input and response functional.

--- react-chatgpt-clone/src/App.js
+++ react-chatgpt-clone/src/App.js
@@ -1,4 +1,29 @@
+import {useState, useEffect} from 'react';
+
 const App = () => {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      console.log(data)
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+console.log(message)
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -16,8 +41,8 @@ const App = () => {
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input />
-            <div id="submit">➢</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>➢</div>
           </div>
           <p className="info">
             This is a simple clone of ChatGPT utilizing the OpenAI API
